ddd21baf9f3993207b8ab2eacc0d1259996a3586
madebygrey <madebygrey5@gmail.com>
2024-01-18 11:45:21 +0200

    add aside & navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-weather-app';
+  showMenu = false;
+  darkModeActive: boolean = false;
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+  modeToggleSwitch() {
+    console.log('switched');
+  }
 }
